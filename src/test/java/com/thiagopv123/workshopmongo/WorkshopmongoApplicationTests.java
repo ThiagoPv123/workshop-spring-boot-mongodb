5dba357f3239c8e16fefa0b1286e728600b3cc5e
Thiago Viana <thiagopetenusso45@gmail.com>
2025-06-02 15:23:19 -0300

    Projeto criado e entity user e rest funcionando

---
+++ src/test/java/com/thiagopv123/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagopv123.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
