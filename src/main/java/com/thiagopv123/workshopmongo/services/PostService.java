7b53d815fb85d8b14f52f2ffd68c2855e24d2c36
Thiago Viana <thiagopetenusso45@gmail.com>
2025-06-10 16:45:46 -0300

    Consulta com vários critérios

--- src/main/java/com/thiagopv123/workshopmongo/services/PostService.java
+++ src/main/java/com/thiagopv123/workshopmongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.thiagopv123.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,10 @@ public class PostService {
     }
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 
 }
